8ae10c982866e86d66cba99d983d5179fb591d06
Rainer Hermanns <hermanns@aixcept.de>
2019-03-06 21:57:57 +0100

    added aws icon to segment

--- segment-aws.go
+++ segment-aws.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"os"
 )
 
 func segmentAWS(p *powerline) {
 	profile, _ := os.LookupEnv("AWS_PROFILE")
 	if profile != "" {
+
 		p.appendSegment("aws", segment{
-			content:    profile,
+			content:    fmt.Sprintf("☁️  %s", profile),
 			foreground: p.theme.AWSFg,
 			background: p.theme.AWSBg,
 		})
