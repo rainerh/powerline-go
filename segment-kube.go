7774d7fb72bcca3288a83a0565673b5038cb7e3e
Rainer Hermanns <hermanns@aixcept.de>
2019-03-12 13:46:44 +0100

    Fixing empty current context issue

--- segment-kube.go
+++ segment-kube.go
@@ -72,72 +72,74 @@ func segmentKube(p *powerline) {
 		}
 	}
 
-	cluster := ""
-	namespace := ""
-	user := ""
+	if config.CurrentContext != "" {
+		cluster := ""
+		namespace := ""
+		user := ""
 
-	for _, context := range config.Contexts {
-		if context.Name == config.CurrentContext {
-			cluster = context.Context.Cluster
-			namespace = context.Context.Namespace
-			user = context.Context.User
-			break
+		for _, context := range config.Contexts {
+			if context.Name == config.CurrentContext {
+				cluster = context.Context.Cluster
+				namespace = context.Context.Namespace
+				user = context.Context.User
+				break
+			}
 		}
-	}
 
-	// When you use gke your clusters may look something like gke_projectname_availability-zone_cluster-01
-	// instead I want it to read as `cluster-01`
-	// So we remove the first 3 segments of this string, if the flag is set, and there are enough segments
-	if strings.HasPrefix(cluster, "gke") && *p.args.ShortenGKENames {
-		segments := strings.Split(cluster, "_")
-		if len(segments) > 3 {
-			cluster = strings.Join(segments[3:], "_")
+		// When you use gke your clusters may look something like gke_projectname_availability-zone_cluster-01
+		// instead I want it to read as `cluster-01`
+		// So we remove the first 3 segments of this string, if the flag is set, and there are enough segments
+		if strings.HasPrefix(cluster, "gke") && *p.args.ShortenGKENames {
+			segments := strings.Split(cluster, "_")
+			if len(segments) > 3 {
+				cluster = strings.Join(segments[3:], "_")
+			}
 		}
-	}
 
-	// With AWS EKS, cluster names are ARNs; it makes more sense to shorten them
-	// so "eks-infra" instead of "arn:aws:eks:us-east-1:XXXXXXXXXXXX:cluster/eks-infra
-	const arnRegexString string = "^arn:aws:eks:[[:alnum:]-]+:[[:digit:]]+:cluster/(.*)$"
-	arnRe := regexp.MustCompile(arnRegexString)
+		// With AWS EKS, cluster names are ARNs; it makes more sense to shorten them
+		// so "eks-infra" instead of "arn:aws:eks:us-east-1:XXXXXXXXXXXX:cluster/eks-infra
+		const arnRegexString string = "^arn:aws:eks:[[:alnum:]-]+:[[:digit:]]+:cluster/(.*)$"
+		arnRe := regexp.MustCompile(arnRegexString)
 
-	if arnMatches := arnRe.FindStringSubmatch(cluster); arnMatches != nil && *p.args.ShortenEKSNames {
-		cluster = arnMatches[1]
-	}
-
-	p.appendSegment("kube-icon", segment{
-		content:    fmt.Sprintf("⎈"),
-		foreground: p.theme.KubeIconFg,
-		background: p.theme.KubeIconBg,
-	})
-
-	// Only draw the icon once
-	if cluster != "" && getPreference(K8sShowCluster, p.theme.KubeShowCluster) {
-		p.appendSegment("kube-cluster", segment{
-			content:    cluster,
-			foreground: p.theme.KubeClusterFg,
-			background: p.theme.KubeClusterBg,
-		})
-	}
-
-	if getPreference(K8sShowContext, p.theme.KubeShowNamespace) {
-		content := config.CurrentContext
-		if user != "" && user != config.CurrentContext && getPreference(K8sShowUser, p.theme.KubeShowUser) {
-			content = fmt.Sprintf("%s@%s", user, content)
+		if arnMatches := arnRe.FindStringSubmatch(cluster); arnMatches != nil && *p.args.ShortenEKSNames {
+			cluster = arnMatches[1]
 		}
-		p.appendSegment("kube-context", segment{
-			content:    content,
-			foreground: p.theme.KubeContextFg,
-			background: p.theme.KubeContextBg,
-		})
-	}
 
-	if namespace != "" && getPreference(K8sShowNamespace, p.theme.KubeShowNamespace) {
-		content := namespace
-		p.appendSegment("kube-namespace", segment{
-			content:    content,
-			foreground: p.theme.KubeNamespaceFg,
-			background: p.theme.KubeNamespaceBg,
+		p.appendSegment("kube-icon", segment{
+			content:    fmt.Sprintf("⎈"),
+			foreground: p.theme.KubeIconFg,
+			background: p.theme.KubeIconBg,
 		})
+
+		// Only draw the icon once
+		if cluster != "" && getPreference(K8sShowCluster, p.theme.KubeShowCluster) {
+			p.appendSegment("kube-cluster", segment{
+				content:    cluster,
+				foreground: p.theme.KubeClusterFg,
+				background: p.theme.KubeClusterBg,
+			})
+		}
+
+		if getPreference(K8sShowContext, p.theme.KubeShowNamespace) {
+			content := config.CurrentContext
+			if user != "" && user != content && getPreference(K8sShowUser, p.theme.KubeShowUser) {
+				content = fmt.Sprintf("%s@%s", user, content)
+			}
+			p.appendSegment("kube-context", segment{
+				content:    content,
+				foreground: p.theme.KubeContextFg,
+				background: p.theme.KubeContextBg,
+			})
+		}
+
+		if namespace != "" && getPreference(K8sShowNamespace, p.theme.KubeShowNamespace) {
+			content := namespace
+			p.appendSegment("kube-namespace", segment{
+				content:    content,
+				foreground: p.theme.KubeNamespaceFg,
+				background: p.theme.KubeNamespaceBg,
+			})
+		}
 	}
 }
 
